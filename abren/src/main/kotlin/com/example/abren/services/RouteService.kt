e75c2bc179177c8a19993934e59649b1631bec73
Bruktawit <bruktawitteklay23@gmail.com>
2021-07-28 13:40:34 +0300

    service

---
+++ abren/src/main/kotlin/com/example/abren/services/RouteService.kt
@@ -0,0 +1,9 @@
+package com.example.abren.services
+
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+
+@Service
+class RouteService(private val routeRepository: RouteRepository) {
+    fun findAll():
+}
