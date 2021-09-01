5f83639a686c3d0f80ba5280781d11013faf0f68
Bruktawit <bruktawitteklay23@gmail.com>
2021-09-01 23:01:24 +0300

    update driver id

--- src/main/kotlin/com/example/abren/handlers/RouteHandler.kt
+++ src/main/kotlin/com/example/abren/handlers/RouteHandler.kt
@@ -2,10 +2,13 @@ package com.example.abren.handlers
 
 import com.example.abren.models.Location
 import com.example.abren.models.Route
+import com.example.abren.models.User
 import com.example.abren.services.RouteService
+import com.example.abren.services.UserService
 import org.springframework.http.MediaType
 import org.springframework.http.codec.multipart.FormFieldPart
 import org.springframework.http.codec.multipart.Part
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyExtractor
 import org.springframework.web.reactive.function.BodyExtractors
@@ -20,7 +23,7 @@ import java.time.LocalDateTime
 import javax.print.attribute.standard.Media
 
 @Component
-class RouteHandler (private val routeService: RouteService) {
+class RouteHandler (private val routeService: RouteService,private val userService: UserService) {
 
     fun getRoutes(r: ServerRequest): Mono<ServerResponse> {
         val routesFlux = routeService.findAll()
@@ -45,24 +48,24 @@ class RouteHandler (private val routeService: RouteService) {
     }
 
     fun updateRoute(r: ServerRequest): Mono<ServerResponse> {
-        val toBeUpdatedMono : Mono<Route?> = routeService.findOne(r.pathVariable("id"))
+        val toBeUpdatedMono: Mono<Route?> = routeService.findOne(r.pathVariable("id"))
 
-        return toBeUpdatedMono.flatMap { toBeUpdated->
+        return toBeUpdatedMono.flatMap { toBeUpdated ->
             val routeUpdateRequestMono = r.bodyToMono(Route::class.java)
 
             routeUpdateRequestMono.flatMap { routeUpdateRequest ->
 
-                toBeUpdated?.startingLocation=routeUpdateRequest.startingLocation
-                toBeUpdated?.waypointLocations=routeUpdateRequest.waypointLocations
-                toBeUpdated?.destinationLocation=routeUpdateRequest.destinationLocation
+                toBeUpdated?.startingLocation = routeUpdateRequest.startingLocation
+                toBeUpdated?.waypointLocations = routeUpdateRequest.waypointLocations
+                toBeUpdated?.destinationLocation = routeUpdateRequest.destinationLocation
 
-                toBeUpdated?.updatedAt= LocalDateTime.now()
+                toBeUpdated?.updatedAt = LocalDateTime.now()
 
-                var updatedRouteMono : Mono<Route> = Mono.empty()
-                if(toBeUpdated!= null){
+                var updatedRouteMono: Mono<Route> = Mono.empty()
+                if (toBeUpdated != null) {
                     updatedRouteMono = routeService.update(toBeUpdated)
                 }
-                updatedRouteMono.flatMap { updatedRoute->
+                updatedRouteMono.flatMap { updatedRoute ->
                     ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
                             BodyInserters.fromValue(updatedRoute))
                 }
@@ -70,15 +73,21 @@ class RouteHandler (private val routeService: RouteService) {
         }
     }
 
-fun createRoute(r: ServerRequest): Mono<ServerResponse> {
-    val routeMono = r.bodyToMono(Route::class.java)
-    return routeMono.flatMap { route->
-        route.driverId = "id1"
-        route.createdAt = LocalDateTime.now()
-        val savedRoute = routeService.create(route)
-        ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
-                BodyInserters.fromProducer(savedRoute,Route::class.java))
+    fun createRoute(r: ServerRequest): Mono<ServerResponse> {
+        val routeMono = r.bodyToMono(Route::class.java)
+        return ReactiveSecurityContextHolder.getContext().flatMap { securityContext ->
+            val userMono: Mono<User?> = userService.findByPhoneNumber(securityContext.authentication.principal as String)
+            userMono.flatMap { user ->
+                routeMono.flatMap { route ->
+                    route.driverId = user?._id
+                    route.createdAt = LocalDateTime.now()
+                    val savedRoute = routeService.create(route)
 
+                    ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
+                            BodyInserters.fromProducer(savedRoute, Route::class.java))
+
+                }
+            }
+        }
     }
-}
 }
